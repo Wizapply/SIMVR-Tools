cee0a4461ba4ce09e49f0547c9575e7210ce98d2
Wizapply <info@wizapply.com>
2017-08-07 09:01:54 +0900

    SIMVRMover、アルゴリズム改善とUE4対応

--- build_ue4/Plugins/SIMVRPlugin/Source/SIMVRPlugin/Private/SIMVRMover.cpp
+++ build_ue4/Plugins/SIMVRPlugin/Source/SIMVRPlugin/Private/SIMVRMover.cpp
@@ -10,6 +10,14 @@ USIMVRMover::USIMVRMover()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
+	Controller = nullptr;
+	TrackingTarget = nullptr;
+
+	updateTime = 0.032f;
+	wscale = FVector4(1.0f, 1.0f, 1.0f, 1.0f);
+
+	//private var
+	currTime = 0;
 }
 
 
@@ -17,6 +25,20 @@ USIMVRMover::USIMVRMover()
 void USIMVRMover::BeginPlay()
 {
 	Super::BeginPlay();
+	Controller = GetOwner()->FindComponentByClass<USIMVRComponent>();
+
+	if (Controller == nullptr) {
+		UE_LOG(LogTemp, Warning, TEXT("%s"), "USIMVRComponent Nothing");
+	}
+	if (TrackingTarget == nullptr) {
+		UE_LOG(LogTemp, Warning, TEXT("%s"), "TrackingTarget Nothing");
+	}
+
+	currTime = updateTime;
+	previousPos = TrackingTarget->GetTransform().GetLocation();
+	previousYaw = 0.0f;
+
+	saveRoll = savePitch = saveYaw = saveHeave = saveSway = saveSurge = 0.0f;
 }
 
 void USIMVRMover::EndPlay(const EEndPlayReason::Type EndPlayReason)
@@ -29,4 +51,71 @@ void USIMVRMover::TickComponent( float DeltaTime, ELevelTick TickType, FActorCom
 {
 	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
 
+	if (Controller == nullptr)
+		return;
+	if (TrackingTarget == nullptr)
+		return;
+
+	currTime -= DeltaTime;
+	if (currTime <= 0.0f)
+	{
+		FVector vec = TrackingTarget->GetTransform().GetLocation() - previousPos;
+		previousPos = TrackingTarget->GetTransform().GetLocation();
+
+		//G_calc
+		FVector surge, sway, heave;
+		surge.X = vec.X * TrackingTarget->GetActorForwardVector().X;
+		surge.Y = vec.Y * TrackingTarget->GetActorForwardVector().Y;
+		surge.Z = vec.Z * TrackingTarget->GetActorForwardVector().Z;
+		sway.X = vec.X * TrackingTarget->GetActorRightVector().X;
+		sway.Y = vec.Y * TrackingTarget->GetActorRightVector().Y;
+		sway.Z = vec.Z * TrackingTarget->GetActorRightVector().Z;
+		heave.X = vec.X * TrackingTarget->GetActorUpVector().X;
+		heave.Y = vec.Y * TrackingTarget->GetActorUpVector().Y;
+		heave.Z = vec.Z * TrackingTarget->GetActorUpVector().Z;
+
+		//G
+		saveSurge = Controller->Surge = ToRoundDown((surge.X + surge.Y + surge.Z) * wscale.X, 2);
+		saveSway = Controller->Sway = ToRoundDown((sway.X + sway.Y + sway.Z) * wscale.Y, 2);
+		saveHeave = Controller->Heave = ToRoundDown((heave.X + heave.Y + heave.Z) * wscale.Z, 2);
+
+		//YAW_G calc
+		float yaws = FMath::FindDeltaAngle(TrackingTarget->GetTransform().GetRotation().Euler().Y, previousYaw) / 10.0f * wscale.W;	//+-10“x = +-1.0
+		previousYaw = TrackingTarget->GetTransform().GetRotation().Euler().Y;
+		//YAW_G
+		saveYaw = Controller->Yaw = ToRoundDown(yaws, 2);
+
+		//Roll, Pitch
+		float rolls = FMath::FindDeltaAngle(TrackingTarget->GetTransform().GetRotation().Euler().Z, 0.0f) / 10.0f;	//+-10“x
+		float pitchs = FMath::FindDeltaAngle(TrackingTarget->GetTransform().GetRotation().Euler().X, 0.0f) / 10.0f;	//+-10“x
+
+		saveRoll = Controller->Roll = ToRoundDown(rolls, 2);
+		savePitch = Controller->Pitch = ToRoundDown(pitchs, 2);
+
+		currTime = updateTime;
+	}
+	else {
+		Controller->Roll = saveRoll;
+		Controller->Pitch = savePitch;
+		Controller->Yaw = saveYaw;
+		Controller->Heave = saveHeave;
+		Controller->Sway = saveSway;
+		Controller->Surge = saveSurge;
+	}
+
+	Controller->Roll += RollForce;
+	Controller->Pitch += PitchForce;
+	Controller->Yaw += YawForce;
+	Controller->Heave += HeaveForce;
+	Controller->Sway += SwayForce;
+	Controller->Surge += SurgeForce;
+
+}
+
+float USIMVRMover::ToRoundDown(float dValue, int iDigits)
+{
+	float dCoef = FMath::Pow(10, iDigits);
+
+	return dValue > 0 ? FMath::Floor(dValue * dCoef) / dCoef :
+		FMath::Ceil(dValue * dCoef) / dCoef;
 }
--- build_ue4/Plugins/SIMVRPlugin/Source/SIMVRPlugin/Public/SIMVRMover.h
+++ build_ue4/Plugins/SIMVRPlugin/Source/SIMVRPlugin/Public/SIMVRMover.h
@@ -3,12 +3,30 @@
 #pragma once
 
 #include "Components/ActorComponent.h"
+#include "SIMVRComponent.h"
 #include "SIMVRMover.generated.h"
 
 UCLASS( ClassGroup=(SIMVRMover), meta=(BlueprintSpawnableComponent) )
 class USIMVRMover : public UActorComponent
 {
 	GENERATED_BODY()
+
+private:
+	USIMVRComponent* Controller;
+
+	float currTime;
+	FVector previousPos;
+	float previousYaw;
+
+	float saveRoll;
+	float savePitch;
+	float saveYaw;
+	float saveHeave;
+	float saveSway;
+	float saveSurge;
+
+	//private method
+	float ToRoundDown(float dValue, int iDigits);
 
 public:	
 	// Sets default values for this component's properties
@@ -21,5 +39,27 @@ public:
 
 	// Called every frame
 	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+
+	UPROPERTY(Category = "Target Setting", EditAnywhere)
+		AActor* TrackingTarget;
+	UPROPERTY(Category = "Processing Setting", EditAnywhere, meta = (ClampMin = "0.0"))
+		float updateTime;
+	UPROPERTY(Category = "Processing Setting", EditAnywhere)
+		FVector4 wscale;
+
+	//Rotation
+	UPROPERTY(Category = "Force Rotation and G", EditAnywhere, meta = (ClampMin = "-1.0", ClampMax = "1.0", UIMin = "-1.0", UIMax = "1.0"))
+		float RollForce;
+	UPROPERTY(Category = "Force Rotation and G", EditAnywhere, meta = (ClampMin = "-1.0", ClampMax = "1.0", UIMin = "-1.0", UIMax = "1.0"))
+		float PitchForce;
+	UPROPERTY(Category = "Force Rotation and G", EditAnywhere, meta = (ClampMin = "-1.0", ClampMax = "1.0", UIMin = "-1.0", UIMax = "1.0"))
+		float YawForce;
+	//G
+	UPROPERTY(Category = "Force Rotation and G", EditAnywhere, meta = (ClampMin = "-1.0", ClampMax = "1.0", UIMin = "-1.0", UIMax = "1.0"))
+		float HeaveForce;
+	UPROPERTY(Category = "Force Rotation and G", EditAnywhere, meta = (ClampMin = "-1.0", ClampMax = "1.0", UIMin = "-1.0", UIMax = "1.0"))
+		float SwayForce;
+	UPROPERTY(Category = "Force Rotation and G", EditAnywhere, meta = (ClampMin = "-1.0", ClampMax = "1.0", UIMin = "-1.0", UIMax = "1.0"))
+		float SurgeForce;
 
 };
